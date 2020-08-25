eb2ade670345a43c6a5cb87745371ee28bc1fb62
matt-labor <matao11235@163.com>
2020-08-25 22:30:00 +0800

    not finished on work PC0825

--- collector.py
+++ collector.py
@@ -1,22 +1,50 @@
 import logging
 import time
+
 import os
-#import epics
+# import epics
 
 from datetime import datetime, timedelta
 from prometheus_client import Summary
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 
 
+class PvCollector(object):
+    def __init__(self, config):
+        # self.
+        self.systems = config['system']
+        for value in self.systems:
+            # system = value
+            for alert_item in value:
+                for pvs in alert_item:
+                    alerts = AlertItem(value, alert_item, pvs)
 
-class CollectorConfig(object):
-    def __init__(self):
+
+class AlertItem():
+    def __init__(self, system, alert_item, pvs):
+        self.system = system
+        self.alert_item = alert_item
+        self.pvs = pvs
+        self.last_value = None
+        for pv in pvs:
+            self.registerPV(pv,alert_item,system)
+
+    def registerPV(self,pv=None, alert_item=None, system=None):
+        regpv = epics.PV(pv)
+        regpv.add_callback(self.onChanges)
+
+    def onChanges(self, pv=None, value=None, char_value=None, **kw):
+
+        self.last_value = value
+        logger.info("PV " + pvname + " value changed to " + str(value))
+
+
+
+
+
+    def collect(self):
         pass
 
-
-class PvCollector():
-    def __init__(self):
-        pass
 
 
 
